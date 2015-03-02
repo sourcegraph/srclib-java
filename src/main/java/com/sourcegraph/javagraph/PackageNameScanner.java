846a0c264e105704afc34b537895dd6133dfc6d5
Quinn Slack <qslack@qslack.com>
2015-03-02 06:01:42 -0800

    dont emit prefix package name paths, only resolvable ones

--- src/main/java/com/sourcegraph/javagraph/PackageNameScanner.java
+++ src/main/java/com/sourcegraph/javagraph/PackageNameScanner.java
@@ -1,16 +1,23 @@
 package com.sourcegraph.javagraph;
 
 import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePathScanner;
 
 public abstract class PackageNameScanner extends TreePathScanner<Void, Void> {
     @Override
     public Void visitIdentifier(IdentifierTree node, Void p) {
-        writePackageName(node.toString(), node.toString(), node);
         super.visitIdentifier(node, p);
         return null;
     }
+
+    @Override
+    public Void visitMemberSelect(MemberSelectTree node, Void p) {
+        writePackageName(node.toString(), node.getIdentifier().toString(), node);
+        return null;
+    }
+
 
     public abstract void writePackageName(String qualName, String simpleName,
                                           Tree node);
