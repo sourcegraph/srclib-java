6aaef23da912c4d491048fb49867b21cddfc97b3
Alex Saveliev <lyolik@codeminders.com>
2015-08-05 11:14:03 +0300

    OpenJDK support
    - compiling all OpenJDK Java8 projects using the same rules
    - putting list of source directories to file manager locations

--- src/main/java/com/sourcegraph/javagraph/JDKProject.java
+++ src/main/java/com/sourcegraph/javagraph/JDKProject.java
@@ -3,30 +3,70 @@ package com.sourcegraph.javagraph;
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.model.building.ModelBuildingException;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JDKProject implements Project {
+
     private SourceUnit unit;
 
     public JDKProject(SourceUnit unit) {
-        this.unit=unit;
+        this.unit = unit;
     }
 
     @Override
     public List<String> getBootClassPath() throws Exception {
+        if (unit.Repo.equals(ScanCommand.JDK_REPO)) {
+            return Collections.emptyList();
+        }
         return null;
     }
 
     @Override
     public List<String> getClassPath() throws Exception {
+        File javaHome = SystemUtils.getJavaHome();
+        if (javaHome.isDirectory()) {
+            File toolsJar = new File(javaHome, "lib/tools.jar");
+            if (toolsJar.isFile()) {
+                return Collections.singletonList(toolsJar.getAbsolutePath());
+            }
+            // JAVA_HOME may point to JRE, trying one level up
+            toolsJar = new File(javaHome, "../lib/tools.jar");
+            if (toolsJar.isFile()) {
+                return Collections.singletonList(toolsJar.getAbsolutePath());
+            }
+        }
         return null;
     }
 
     @Override
     public List<String> getSourcePath() throws Exception {
-        return null;
+        List<String> sourcePaths = new ArrayList<>();
+        sourcePaths.add("src/share/classes/");
+        sourcePaths.add("src/share/jaxws_classes/");
+        sourcePaths.add("src/share/jaf_classes/");
+        sourcePaths.add("src/share/");
+        sourcePaths.add("src/");
+
+        if (SystemUtils.IS_OS_WINDOWS) {
+            sourcePaths.add("src/windows/classes/");
+        } else if (SystemUtils.IS_OS_MAC_OSX) {
+            sourcePaths.add("src/macosx/classes/");
+        } else if (SystemUtils.IS_OS_LINUX) {
+            sourcePaths.add("src/linux/classes/");
+        } else if (SystemUtils.IS_OS_FREE_BSD || SystemUtils.IS_OS_NET_BSD || SystemUtils.IS_OS_OPEN_BSD) {
+            sourcePaths.add("src/bsd/classes/");
+        } else if (SystemUtils.IS_OS_SOLARIS) {
+            sourcePaths.add("src/solaris/classes/");
+        }
+
+        return sourcePaths.stream().filter(element -> new File(element).isDirectory()).collect(Collectors.toList());
     }
 
     @Override
@@ -48,6 +88,14 @@ public class JDKProject implements Project {
     private static List<String> getJDKSourcePaths() throws Exception {
         List<String> sourcePaths = new ArrayList<>();
         sourcePaths.add("src/share/classes/");
+        sourcePaths.add("src/share/jaxws_classes/");
+        sourcePaths.add("src/share/jaf_classes/");
+        sourcePaths.add("src/share/vm/");
+        sourcePaths.add("src/share/tools/");
+        sourcePaths.add("src/org/");
+        sourcePaths.add("src/javax/");
+        sourcePaths.add("src/com/");
+        sourcePaths.add("src/jdk/");
 
         if (SystemUtils.IS_OS_WINDOWS) {
             sourcePaths.add("src/windows/classes/");
@@ -61,7 +109,7 @@ public class JDKProject implements Project {
             sourcePaths.add("src/solaris/classes/");
         }
 
-        return sourcePaths;
+        return sourcePaths.stream().filter(element -> new File(element).isDirectory()).collect(Collectors.toList());
     }
 
     public static Collection<SourceUnit> standardSourceUnits() throws Exception {
@@ -76,14 +124,23 @@ public class JDKProject implements Project {
         unit.Data.put("JDK", true);
         units.add(unit);
 
-        // Build tools source unit
-        final SourceUnit toolsUnit = new SourceUnit();
-        toolsUnit.Type = "JavaArtifact";
-        toolsUnit.Name = "BuildTools";
-        toolsUnit.Dir = "make/src/classes/";
-        toolsUnit.Files = ScanUtil.scanFiles("make/src/classes/");
-        units.add(toolsUnit);
+        addKnownSourceUnit(units, "make/src/classes/");
+        addKnownSourceUnit(units, "buildtools/nasgen/");
 
         return units;
+    }
+
+    private static void addKnownSourceUnit(Collection<SourceUnit> units, String directory) throws IOException {
+        File dir = new File(directory);
+        if (dir.isDirectory()) {
+            // Build tools source unit
+            final SourceUnit toolsUnit = new SourceUnit();
+            toolsUnit.Type = "JavaArtifact";
+            toolsUnit.Name = "BuildTools";
+            toolsUnit.Dir = directory;
+            toolsUnit.Files = ScanUtil.scanFiles(directory);
+            toolsUnit.Data.put("JDK", true);
+            units.add(toolsUnit);
+        }
     }
 }
