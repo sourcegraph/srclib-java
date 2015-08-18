25e2ab7d03083ebabf621b23d0d7093aea582eea
Alex Saveliev <lyolik@codeminders.com>
2015-08-18 12:41:48 +0300

    Maven support
    - since v3.3 Maven Windows command is mvn.cmd (was mvn.bat before). Running "mvn -v" to verify which one to use
    - Added support of antl4 Maven plugin. When found, generate-source goal is executed before scanning for sources to generate java files from grammar

--- src/main/java/com/sourcegraph/javagraph/maven/plugins/AbstractMavenPlugin.java
+++ src/main/java/com/sourcegraph/javagraph/maven/plugins/AbstractMavenPlugin.java
@@ -1,15 +1,25 @@
 package com.sourcegraph.javagraph.maven.plugins;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public abstract class AbstractMavenPlugin implements MavenPlugin {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractMavenPlugin.class);
+
     public abstract String getGroupId();
 
     public abstract String getArtifactId();
+
+    private static String mavenCmd;
 
     public boolean isApplicable(MavenProject project) {
         return getPlugin(project) != null;
@@ -28,5 +38,59 @@ public abstract class AbstractMavenPlugin implements MavenPlugin {
         }
         return null;
     }
+
+    protected static void runMavenGoal(File pomFile, File repoDir, String goal) {
+
+        String cmd[] = new String[] {
+                getMavenCmd(),
+                "-Dmaven.repo.local=" + repoDir,
+                "-f", pomFile.getAbsolutePath(),
+                goal
+        };
+
+        ProcessBuilder pb = new ProcessBuilder(cmd);
+        pb.directory(pomFile.getParentFile());
+        pb.redirectErrorStream(true);
+        File log = null;
+        try {
+            log = File.createTempFile("srclib-mvn", "log");
+            pb.redirectOutput(log);
+
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Starting {} and logging results into {}", cmd, log);
+            }
+            Process p = pb.start();
+            int status = p.waitFor();
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Maven output was {}", FileUtils.readFileToString(log));
+            }
+            if (status != 0) {
+                LOGGER.warn("Exit status of {} was {}", cmd, status);
+            }
+        } catch (IOException | InterruptedException e) {
+            LOGGER.warn("Unable to compile Maven project using command {}", cmd, e);
+        } finally {
+            FileUtils.deleteQuietly(log);
+        }
+    }
+
+    protected static String getMavenCmd() {
+        if (mavenCmd == null) {
+            if (SystemUtils.IS_OS_WINDOWS) {
+                // since 3.3 command name is mvn.cmd, before - mvn.bat
+                // let's try to run mvn -v and see if mvn.cmd works
+                try {
+                    Runtime.getRuntime().exec(new String[] {"mvn.cmd",  "-v"});
+                    mavenCmd = "mvn.cmd";
+                } catch (IOException e) {
+                    mavenCmd = "mvn.bat";
+                }
+            } else {
+                mavenCmd = "mvn";
+            }
+        }
+        return mavenCmd;
+    }
+
 
 }
--- src/main/java/com/sourcegraph/javagraph/maven/plugins/SimpligilityAndroidMavenPlugin.java
+++ src/main/java/com/sourcegraph/javagraph/maven/plugins/SimpligilityAndroidMavenPlugin.java
@@ -2,23 +2,16 @@ package com.sourcegraph.javagraph.maven.plugins;
 
 import com.sourcegraph.javagraph.PathUtil;
 import com.sourcegraph.javagraph.Project;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
 import org.apache.maven.project.MavenProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.IOException;
 
 /**
  * Adds basic support of http://simpligility.github.io/android-maven-plugin/index.html
  * Generates source files and updates compile source roots to include directories with generated files
  */
 public class SimpligilityAndroidMavenPlugin extends AbstractMavenPlugin {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SimpligilityAndroidMavenPlugin.class);
 
     @Override
     public String getGroupId() {
@@ -34,51 +27,9 @@ public class SimpligilityAndroidMavenPlugin extends AbstractMavenPlugin {
     public void apply(MavenProject project,
                       File repoDir) {
         // Let's create generated source file such as R.java, BuildConfig.java and AIDL-based ones
-        doGenerateSources(project.getModel().getPomFile(), repoDir);
+        runMavenGoal(project.getModel().getPomFile(), repoDir, "generate-test-sources");
         project.getCompileSourceRoots().add(getGeneratedSourceDirectory(project));
         project.getProperties().setProperty(Project.ANDROID_PROPERTY, StringUtils.EMPTY);
-    }
-
-    private static void doGenerateSources(File pomFile, File repoDir) {
-
-        String mvnCmd;
-        if (SystemUtils.IS_OS_WINDOWS) {
-            mvnCmd = "mvn.bat";
-        } else {
-            mvnCmd = "mvn";
-        }
-        String cmd[] = new String[] {
-                mvnCmd,
-                "-Dmaven.repo.local=" + repoDir,
-                "-f", pomFile.getAbsolutePath(),
-                "generate-test-sources"
-        };
-
-        // let's execute "mvn generate-test-sources" that will do the job
-        ProcessBuilder pb = new ProcessBuilder(cmd);
-        pb.directory(pomFile.getParentFile());
-        pb.redirectErrorStream(true);
-        File log = null;
-        try {
-            log = File.createTempFile("srclib-mvn", "log");
-            pb.redirectOutput(log);
-
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Starting {} and logging results into {}", (Object[]) cmd, log);
-            }
-            Process p = pb.start();
-            int status = p.waitFor();
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Maven output was {}", FileUtils.readFileToString(log));
-            }
-            if (status != 0) {
-                LOGGER.warn("Exit status of {} was {}", cmd, status);
-            }
-        } catch (IOException | InterruptedException e) {
-            LOGGER.warn("Unable to compile Maven project using command {}", cmd, e);
-        } finally {
-            FileUtils.deleteQuietly(log);
-        }
     }
 
     private String getGeneratedSourceDirectory(MavenProject project) {
