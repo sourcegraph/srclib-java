c0b2978f0709489a4718fe859a9c68196764a893
Alex Saveliev <lyolik@codeminders.com>
2015-08-20 14:07:14 +0300

    Improvement of package origin resolution
    - When looking for package origin, we came from class node for sure, thus we don't need to retrieve all enclosed package elements (which may product bad class errors), we can simply kep reference to java file object of last visited class node and return it when asked for package's java file object

--- src/main/java/com/sourcegraph/javagraph/Origins.java
+++ src/main/java/com/sourcegraph/javagraph/Origins.java
@@ -1,17 +1,13 @@
 package com.sourcegraph.javagraph;
 
 import com.sun.tools.javac.code.Symbol.ClassSymbol;
-import com.sun.tools.javac.jvm.ClassReader.BadClassFile;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.lang.model.element.Element;
-import javax.lang.model.element.PackageElement;
 import javax.tools.JavaFileObject;
 
 public class Origins {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Origins.class);
+    private static JavaFileObject lastElementObject;
 
     public static JavaFileObject forElement(Element e) {
         switch (e.getKind()) {
@@ -21,29 +17,15 @@ public class Origins {
             case ANNOTATION_TYPE:
                 return forClass((ClassSymbol) e);
             case PACKAGE:
-                return forPackage((PackageElement) e);
+                return lastElementObject;
             default:
                 return forElement(e.getEnclosingElement());
         }
     }
 
     public static JavaFileObject forClass(ClassSymbol s) {
+        lastElementObject = s.classfile;
         return s.classfile;
     }
 
-    public static JavaFileObject forPackage(PackageElement s) {
-        // Packages can be defined by multiple JARs, but let's just take the JAR
-        // that defines this package's first element.
-        try {
-            for (Element e : s.getEnclosedElements()) {
-                JavaFileObject o = forElement(e);
-                if (o != null) {
-                    return o;
-                }
-            }
-        } catch (BadClassFile ex) {
-            LOGGER.warn("Bad class file", ex);
-        }
-        return null;
-    }
 }
