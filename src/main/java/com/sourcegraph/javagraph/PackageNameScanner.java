a51fdf74b2a1017da6bdbea9caf8163841f7ec57
Alex Saveliev <lyolik@codeminders.com>
2016-02-15 15:32:30 +0200

    Better fix for sourcegraph/srclib-java#23
    - covering cases when there is a whitespace between var and member, for example ("foo .bar")
    - covering cases when there is a generic between var and member, for example ("Collections.<String>emptyList")

--- src/main/java/com/sourcegraph/javagraph/PackageNameScanner.java
+++ src/main/java/com/sourcegraph/javagraph/PackageNameScanner.java
@@ -18,11 +18,7 @@ public abstract class PackageNameScanner extends TreePathScanner<Void, Void> {
     @Override
     public Void visitMemberSelect(MemberSelectTree node, Void p) {
         if (SourceVersion.isIdentifier(node.getIdentifier())) {
-            try {
-                writePackageName(node.toString(), node.getIdentifier().toString(), node);
-            } catch (Spans.SpanException e) {
-                LOGGER.warn("Span exception", e);
-            }
+            writePackageName(node.toString(), node.getIdentifier().toString(), node);
         }
         return null;
     }
